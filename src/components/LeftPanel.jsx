30201105637909284cdc768e421f9754dbdd535f
farazazeem-6 <faraz.tacticminds.com>
2025-10-06 23:12:51 +0500

    left-panel-done

--- src/components/LeftPanel.jsx
+++ src/components/LeftPanel.jsx
@@ -1,70 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/LeftPanel.css";
 import AirIcon from "@mui/icons-material/Air";
 import WaterDropIcon from "@mui/icons-material/WaterDrop";
 
-function LeftPanel() {
+function LeftPanel({ weather, onCityChange, dayForcast }) {
+  const [inputValue, setInputValue] = useState("");
+
+const handleSearch = () => {
+  if (inputValue.trim()) {
+    onCityChange(inputValue);
+    setInputValue("");
+  }
+};
+
+const handleKeyDown = (e) => {
+  if (e.key === "Enter") {
+    handleSearch();
+  }
+};
+
+  if (!weather || Object.keys(weather).length === 0) {
+    return <div className="left-panel">Loading...</div>;
+  }
+  if (!dayForcast || Object.keys(dayForcast).length === 0) {
+    return <div className="left-panel">Loading...</div>;
+  }
+  console.log(dayForcast);
+
+  function getShortDayName(dateString) {
+    const cleanDate = dateString.split(":")[0];
+    const date = new Date(cleanDate);
+    return date.toLocaleDateString("en-US", { weekday: "short" });
+  }
+  function extractDate(datetimeString) {
+    return datetimeString.split(" ")[0];
+  }
+
   return (
     <div className="left-panel">
       <div className="header">
-        <p className="city-name">Multan</p>
-        <p className="date">06-09-2025</p>
+        <p className="city-name">{weather.city_name}</p>
+        <p className="date">{extractDate(weather.ob_time)}</p>
         <div className="search-bar">
-          <input type="text" placeholder="Enter City" />
-          <button className="search-btn">Search</button>
+          <input
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            type="text"
+            placeholder="Enter City"
+          />
+          <button onClick={handleSearch} className="search-btn">Search</button>
         </div>
       </div>
       <div className="left-panel-body">
         <div className="temp-section">
-          <p className="act-temp">20°</p>
-          <p className="weather-type">Cloudy</p>
+          <p className="act-temp">{weather.app_temp}</p>
+          <p className="weather-type">{weather.weather.description}</p>
         </div>
         <div className="extra-detail-section">
           <div className="wind-detail">
             <span>
               <AirIcon />
             </span>
-            <p className="wind">6.1 mph</p>
+            <p className="wind">{weather.wind_spd} mph</p>
           </div>
           <div className="moisture">
             <span>
               <WaterDropIcon />
             </span>
-            <p className="moisture-percentage">90 %</p>
+            <p className="moisture-percentage">{weather.rh} %</p>
           </div>
         </div>
       </div>
       <div className="days-forcast">
-        <div className="day-weather-detail">
-          <div className="day-name">Monday</div>
-          <div className="day-temp">20°</div>
-          <div className="day-type">Rainy</div>
-        </div>
-        <div className="day-weather-detail">
-          <div className="day-name">Monday</div>
-          <div className="day-temp">20°</div>
-          <div className="day-type">Rainy</div>
-        </div>
-        <div className="day-weather-detail">
-          <div className="day-name">Monday</div>
-          <div className="day-temp">20°</div>
-          <div className="day-type">Rainy</div>
-        </div>
-        <div className="day-weather-detail">
-          <div className="day-name">Monday</div>
-          <div className="day-temp">20°</div>
-          <div className="day-type">Rainy</div>
-        </div>
-        <div className="day-weather-detail">
-          <div className="day-name">Monday</div>
-          <div className="day-temp">20°</div>
-          <div className="day-type">Rainy</div>
-        </div>
-        <div className="day-weather-detail">
-          <div className="day-name">Monday</div>
-          <div className="day-temp">20°</div>
-          <div className="day-type">Rainy</div>
-        </div>
+        {dayForcast.map((item, index) => (
+          <div className="day-weather-detail" key={index}>
+            <div className="day-name">{getShortDayName(item.datetime)}</div>
+            <div className="day-temp">{item.app_min_temp}°</div>
+            <div className="day-type">{item.weather.description}</div>
+          </div>
+        ))}
       </div>
     </div>
   );
