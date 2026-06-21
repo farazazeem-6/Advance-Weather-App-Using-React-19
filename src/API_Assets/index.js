08e26ed6aea92d42ea588815e43669573df11a57
faraz-dhillon <faraz.tacticminds@gmail.com>
2026-06-21 17:34:34 +0500

    fix the base url

--- src/API_Assets/index.js
+++ src/API_Assets/index.js
@@ -1,14 +1,13 @@
 export const LatLongApi =
-    "https://nominatim.openstreetmap.org/search?city=Faisalabad&format=json";
+  "https://nominatim.openstreetmap.org/search?city=Faisalabad&format=json";
 
 //////////////////////
 // Weatherbit API
 
-export const currWeather =
-    "https://api.weatherbit.io/v2.0/current?city=Seattle&key=63c7960e2af24fe2a62e376c3a2a9e91";
+export const currWeather = `${BASE_URL}current?city=Seattle&key=63c7960e2af24fe2a62e376c3a2a9e91`;
 
-export const hourForcast =
-    "https://api.weatherbit.io/v2.0/forecast/hourly?city=Seattle&hours=48&key=63c7960e2af24fe2a62e376c3a2a9e91";
+export const hourForcast = `${BASE_URL}forecast/hourly?city=Seattle&hours=48&key=63c7960e2af24fe2a62e376c3a2a9e91`;
 
-export const dayForcast =
-    "https://api.weatherbit.io/v2.0/forecast/daily?city=Seattle&days=6&key=63c7960e2af24fe2a62e376c3a2a9e91";
+export const dayForcast = `${BASE_URL}forecast/daily?city=Seattle&days=6&key=63c7960e2af24fe2a62e376c3a2a9e91`;
+
+export const BASE_URL = "https://api.weatherbit.io/v2.0/";
