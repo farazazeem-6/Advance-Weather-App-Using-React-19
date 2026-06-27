7a19385ab32f85612926d01e3161db6425ea0354
faraz-dhillon <faraz.tacticminds@gmail.com>
2026-06-27 23:05:01 +0500

    add base url

--- src/API_Assets/index.js
+++ src/API_Assets/index.js
@@ -1,7 +1,6 @@
 export const LatLongApi =
   "https://nominatim.openstreetmap.org/search?city=Faisalabad&format=json";
 
-//////////////////////
 // Weatherbit API
 
 export const currWeather = `${BASE_URL}current?city=Seattle&key=63c7960e2af24fe2a62e376c3a2a9e91`;
